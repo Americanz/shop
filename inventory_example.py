89c6180301b18f3e943aebc74463f6e1fa43a348
Vadym <v.gavruk@gmail.com>
2024-03-21 13:44:20 +0200

    n

--- inventory_example.py
+++ inventory_example.py
@@ -391,7 +391,7 @@ def inventory_input(hashMap, _files=None, _data=None):
                     "height": "wrap_content",
                     "width": "match_parent",
                     "weight": "0",
-                    "Value": name,
+                    "Value": "name: " + name,
                     "Variable": "",
                 },
                 {
