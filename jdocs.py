8b4a9caa6a0e41fcee0a0d1a479f15e444bba81e
Vadym <v.gavruk@gmail.com>
2024-03-21 17:35:49 +0200

    inventory_example

--- jdocs.py
+++ jdocs.py
@@ -82,3 +82,109 @@ jdocs = {
         },
     }
 }
+
+
+
+{
+   "customcards":{
+      "options":{
+         "search_enabled":true,
+         "save_position":true
+      },
+      "layout":{
+         "Value":"",
+         "Variable":"",
+         "type":"LinearLayout",
+         "weight":"0",
+         "height":"wrap_content",
+         "width":"match_parent",
+         "orientation":"vertical",
+         "Elements":[
+            {
+               "type":"LinearLayout",
+               "height":"wrap_content",
+               "width":"match_parent",
+               "weight":"0",
+               "Value":"",
+               "Variable":"",
+               "orientation":"horizontal",
+               "Elements":[
+                  {
+                     "type":"CheckBox",
+                     "Value":"@cb1",
+                     "Variable":"cb1",
+                     "TextSize":"32",
+                     "NoRefresh":false,
+                     "document_type":"",
+                     "mask":"",
+                     "BackgroundColor":"#6750A4",
+                     "width":"match_parent",
+                     "height":"wrap_content",
+                     "weight":2
+                  },
+                  {
+                     "type":"TextView",
+                     "height":"wrap_content",
+                     "width":"wrap_content",
+                     "weight":"0",
+                     "Value":"@type",
+                     "Variable":"",
+                     "TextSize":18                  }
+
+               ]
+
+            {
+               "type":"LinearLayout",
+               "height":"wrap_content",
+               "width":"wrap_content",
+               "weight":"0",
+               "Value":"",
+               "Variable":"",
+               "orientation":"horizontal",
+               "Elements":[
+                  {
+                     "type":"TextView",
+                     "height":"wrap_content",
+                     "width":"wrap_content",
+                     "weight":"0",
+                     "Value":"@number",
+                     "Variable":"",
+                     "TextBold":true,
+                     "TextSize":20
+                  }
+               ]
+            },
+            {
+               "type":"LinearLayout",
+               "height":"wrap_content",
+               "width":"wrap_content",
+               "weight":"0",
+               "Value":"",
+               "Variable":"",
+               "orientation":"vertical",
+               "Elements":[
+                  {
+                     "type":"TextView",
+                     "height":"wrap_content",
+                     "width":"wrap_content",
+                     "weight":"0",
+                     "Value":"@countragent",
+                     "Variable":"",
+                     "TextSize":16
+                  },
+                  {
+                     "type":"TextView",
+                     "height":"wrap_content",
+                     "width":"wrap_content",
+                     "weight":"0",
+                     "Value":"@warehouse",
+                     "Variable":"",
+                     "TextSize":16
+                  }
+               ]
+            }
+         },
+         ]
+      }
+   }
+}
